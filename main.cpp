f355e28ad977a3f6f38ee14f7c2032568e8a1b35
Stefan Hagen <stefan@hagen.link>
2021-09-14 13:06:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+#include <string>
+
+#include <yaml-cpp/yaml.h>
+
+auto CONFIG = R"(---
+- just: write some
+- yaml: 
+  - [here, and]
+  - {it: has, a: 42}
+)";
+
+int main() {
+    YAML::Node config = YAML::Load(CONFIG);
+    std::cout << "Parsed YAML:\n" << config << "\n";
+}
